f3eeb5c7357068fa4bc54f5f2829490960dd74d5
FortimusPrime <adhelesteban@gmail.com>
2025-02-27 09:29:25 -0400

    Adapt Code to run with CMake and NVCC

--- Linear_Algebra/linearAlgebraFunctions.cpp
+++ Linear_Algebra/linearAlgebraFunctions.cpp
@@ -1,72 +1,71 @@
 #include "linearAlgebraFunctions.h"
 
-Vector3D getCrossProduct(Vector3D a, Vector3D b){
-    double cx = a.getY() * b.getZ() - a.getZ() * b.getY();
-    double cy = a.getZ() * b.getX() - a.getX() * b.getZ();
-    double cz = a.getX() * b.getY() - a.getY() * b.getX();
-    return Vector3D(cx, cy, cz);
+Vector3D getCrossProduct(Vector3D a, Vector3D b) {
+  double cx = a.getY() * b.getZ() - a.getZ() * b.getY();
+  double cy = a.getZ() * b.getX() - a.getX() * b.getZ();
+  double cz = a.getX() * b.getY() - a.getY() * b.getX();
+  return Vector3D(cx, cy, cz);
 }
 
-double generateRandomNumber(double min, double max){
-    // Set up random number generator
-    random_device rd; // Seed
-    mt19937 gen(rd()); // Mersenne Twister engine for generating random numbers
-    uniform_int_distribution<> distrib(min, max); // Uniform distribution within [min, max]
+double generateRandomNumber(double min, double max) {
+  // Set up random number generator
+  random_device rd;   // Seed
+  mt19937 gen(rd());  // Mersenne Twister engine for generating random numbers
+  uniform_int_distribution<> distrib(
+      min, max);  // Uniform distribution within [min, max]
 
-    // Generate a random number within the range [min, max]
-    double random_number = distrib(gen);
-    return random_number;
+  // Generate a random number within the range [min, max]
+  double random_number = distrib(gen);
+  return random_number;
 }
 
-Vector3D ColorMax(Vector3D color){
-    array<double, 3> colorArray = {color.getX(), color.getY(), color.getZ()};
-    for (int i = 0; i < 3; i++){
-        if (colorArray[i] > 255){
-            colorArray[i] = 255;
-        }
-        else if (colorArray[i] < 0){
-            colorArray[i] = 0;
-        }
+Vector3D ColorMax(Vector3D color) {
+  array<double, 3> colorArray = {color.getX(), color.getY(), color.getZ()};
+  for (int i = 0; i < 3; i++) {
+    if (colorArray[i] > 255) {
+      colorArray[i] = 255;
+    } else if (colorArray[i] < 0) {
+      colorArray[i] = 0;
     }
-    Vector3D newColor = Vector3D(colorArray[0], colorArray[1], colorArray[2]);
-    return newColor;
+  }
+  Vector3D newColor = Vector3D(colorArray[0], colorArray[1], colorArray[2]);
+  return newColor;
 }
 
-array<Vector3D, 360> circleCoordinates(){
-    array<Vector3D, 360> list;
-    double x;
-    double y;
-    int angle;
-    double rads;
-    for (angle = 0; angle <= 360; angle++){
-        rads = angle*(3.14/180);
-        x = std::cos(rads);
-        y = std::sin(rads);
-        list[angle] = Vector3D(x*2, y*2, 8);
-    }
-    return list;
+array<Vector3D, 360> circleCoordinates() {
+  array<Vector3D, 360> list;
+  double x;
+  double y;
+  int angle;
+  double rads;
+  for (angle = 0; angle < 360; angle++) {
+    rads = angle * (3.14 / 180);
+    x = std::cos(rads);
+    y = std::sin(rads);
+    list[angle] = Vector3D(x * 2, y * 2, 8);
+  }
+  return list;
 }
 
-double degreesToRadians(double deg){
-    return deg * 3.14/180;
-}
+double degreesToRadians(double deg) { return deg * 3.14 / 180; }
 
-Matrix3D getRotationMatrix(double angleA, double angleB, double angleR){
-    double angleAlpha = degreesToRadians(angleA);
-    double angleBeta = degreesToRadians(angleB);
-    double anglePhi = degreesToRadians(angleR);
-    Vector3D vect1 = Vector3D(
-        cos(angleBeta) * cos(anglePhi), 
-        cos(angleBeta) * sin(anglePhi), 
-        sin(angleBeta) * -1);
-    Vector3D vect2 = Vector3D(
-        sin(angleAlpha) * sin(angleBeta)*cos(anglePhi) - cos(angleAlpha) * sin(anglePhi), 
-        sin(angleAlpha) * sin(angleBeta) * sin(anglePhi) + cos(angleAlpha) * cos(anglePhi), 
-        sin(angleAlpha) * cos(angleBeta));
-    Vector3D vect3 = Vector3D(
-        cos(angleAlpha) * sin(angleBeta) * cos(anglePhi) + sin(angleAlpha) * sin(anglePhi), 
-        cos(angleAlpha) * sin(angleBeta) * sin(anglePhi) - sin(angleAlpha) * cos(anglePhi), 
-        cos(angleAlpha) * cos(angleBeta));
-    Matrix3D rotationMatrix = Matrix3D(vect1, vect2, vect3);
-    return rotationMatrix;
+Matrix3D getRotationMatrix(double angleA, double angleB, double angleR) {
+  double angleAlpha = degreesToRadians(angleA);
+  double angleBeta = degreesToRadians(angleB);
+  double anglePhi = degreesToRadians(angleR);
+  Vector3D vect1 =
+      Vector3D(cos(angleBeta) * cos(anglePhi), cos(angleBeta) * sin(anglePhi),
+               sin(angleBeta) * -1);
+  Vector3D vect2 = Vector3D(sin(angleAlpha) * sin(angleBeta) * cos(anglePhi) -
+                                cos(angleAlpha) * sin(anglePhi),
+                            sin(angleAlpha) * sin(angleBeta) * sin(anglePhi) +
+                                cos(angleAlpha) * cos(anglePhi),
+                            sin(angleAlpha) * cos(angleBeta));
+  Vector3D vect3 = Vector3D(cos(angleAlpha) * sin(angleBeta) * cos(anglePhi) +
+                                sin(angleAlpha) * sin(anglePhi),
+                            cos(angleAlpha) * sin(angleBeta) * sin(anglePhi) -
+                                sin(angleAlpha) * cos(anglePhi),
+                            cos(angleAlpha) * cos(angleBeta));
+  Matrix3D rotationMatrix = Matrix3D(vect1, vect2, vect3);
+  return rotationMatrix;
 }
--- Source_Code/AnimationMain.cpp
+++ Source_Code/AnimationMain.cpp
@@ -1,36 +1,46 @@
-// To Run: clang++ -Xpreprocessor -fopenmp -lomp -I/opt/homebrew/Cellar/libomp/19.1.3/include -L/opt/homebrew/Cellar/libomp/19.1.3/lib simple_parallel.cpp -o parallel_example && ./parallel_example
-//  clang++ -Xpreprocessor -fopenmp -lomp -I/opt/homebrew/Cellar/libomp/19.1.3/include -L/opt/homebrew/Cellar/libomp/19.1.3/lib simple_parallel.cpp -o parallel_example && ./parallel_example
-#include <iostream>
-#include <fstream>
+// To Run: clang++ -Xpreprocessor -fopenmp -lomp
+// -I/opt/homebrew/Cellar/libomp/19.1.3/include
+// -L/opt/homebrew/Cellar/libomp/19.1.3/lib simple_parallel.cpp -o
+// parallel_example && ./parallel_example
+//  clang++ -Xpreprocessor -fopenmp -lomp
+//  -I/opt/homebrew/Cellar/libomp/19.1.3/include
+//  -L/opt/homebrew/Cellar/libomp/19.1.3/lib simple_parallel.cpp -o
+//  parallel_example && ./parallel_example
+#include <time.h>
+
 #include <array>
-#include <vector>
 #include <cmath>
-#include <string>
+#include <fstream>
+#include <iostream>
 #include <memory>
 #include <random>
+#include <string>
+#include <vector>
+
+#include "../Linear_Algebra/Matrix3D.h"
+#include "../Linear_Algebra/Vector3D.h"
 #include "../Linear_Algebra/linearAlgebraFunctions.h"
 #include "../Objects/Primitive.h"
-#include "../Linear_Algebra/Vector3D.h"
-#include "../Linear_Algebra/Matrix3D.h"
 #include "../Objects/Scene.h"
 #include "../Objects/derivedObjects.h"
 #include "../Ray-Tracing/RayTracingFunctions.h"
 using namespace std;
 
 // Global Vars
-int distance_viewport_camera = 1; 
-int inf = 2147483647; 
-int recursion_depth = 5; 
+int distance_viewport_camera = 1;
+int inf = 2147483647;
+int recursion_depth = 0;
 
 // Viewport Dims
-int ViewportWidth = 1; 
-int ViewportHeight = 1; 
+int ViewportWidth = 1;
+int ViewportHeight = 1;
 
 // Canvas Dims
-int CanvasWidth = 800; 
+int CanvasWidth = 800;
 int CanvasHeight = 800;
 
-// These are the canvas coordinates in which the parallel processing will be mapping the color vectors to. Adjust width accordingly. 
+// These are the canvas coordinates in which the parallel processing will be
+// mapping the color vectors to. Adjust width accordingly.
 array<array<string, 800>, 800> map2D;
 string colorToMap;
 
@@ -51,179 +61,210 @@ Vector3D BACKGROUND_COLOR = cyan;
 
 // 0, 1, 8 is the center of the circle
 
+Scene makeScene(Vector3D coords1, Vector3D coords2, Vector3D coords3,
+                double angleInput) {
+  // Create Objects
+  Primitive ground_sphere =
+      Primitive(Vector3D(0, -5050, 100), 5000, green, 0, 0.0);
+  Primitive sphere = Primitive(coords1, 1, blue, 500, 0.3);
 
-Scene makeScene(Vector3D coords1, Vector3D coords2, Vector3D coords3, double angleInput){
-     // Create Objects
-    Primitive ground_sphere = Primitive(Vector3D(0, -5050, 100), 5000, green, 0, 0.0);
-    Primitive sphere = Primitive(coords1, 1, blue, 500, 0.3);
+  array<Primitive, 4> tetra = buildTetrahedron2(Vector3D(255, 255, 0), coords3,
+                                                1, 25, 0.0, angleInput);  // Tip
 
-    array<Primitive, 4> tetra = buildTetrahedron2(Vector3D(255, 255, 0), coords3, 1, 25, 0.0, angleInput); // Tip
+  vector<Primitive> objects;
+  objects.push_back(ground_sphere);
+  objects.push_back(sphere);
 
-    vector<Primitive> objects; 
-    objects.push_back(ground_sphere);
-    objects.push_back(sphere); 
+  for (int i = 0; i <= 3; i++) {
+    objects.push_back(tetra[i]);
+  }
 
-    for (int i = 0; i <= 3; i++){
-        objects.push_back(tetra[i]);
-    }
+  array<Primitive, 12> cube = buildCube(coords2, 0.8, red, angleInput);
+  for (int i = 0; i < 12; i++) {
+    objects.push_back(cube[i]);
+  }
 
-    array<Primitive, 12> cube = buildCube(coords2, 0.8, red, angleInput);
-    for (int i = 0; i < 12; i++){
-        objects.push_back(cube[i]);
-    }
+  Light light1 = Light("ambient", 0.2, Vector3D(0, 0, 0), Vector3D(0, 0, 0));
+  Light light2 = Light("point", 0.6, Vector3D(2, 1, 0), Vector3D(0, 0, 0));
+  Light light3 =
+      Light("directional", 0.2, Vector3D(0, 0, 0), Vector3D(1, 4, 4));
+  Light light4 = Light("point", 0.6, Vector3D(2, 1, 0), Vector3D(0, 0, 0));
+  Light light5 = Light("point", 0.2, Vector3D(-2, 1, 0), Vector3D(0, 0, 0));
 
-    Light light1 = Light("ambient", 0.2, Vector3D(0, 0, 0), Vector3D(0, 0, 0));
-    Light light2 = Light("point", 0.6, Vector3D(2, 1, 0), Vector3D(0, 0, 0));
-    Light light3 = Light("directional", 0.2, Vector3D(0, 0, 0), Vector3D(1, 4, 4));
-    Light light4 = Light("point", 0.6, Vector3D(2, 1, 0), Vector3D(0, 0, 0));
-    Light light5 = Light("point", 0.2, Vector3D(-2, 1, 0), Vector3D(0, 0, 0));
+  vector<Light> lights;
+  lights.push_back(light1);
+  lights.push_back(light2);
+  lights.push_back(light3);
+  lights.push_back(light4);
+  lights.push_back(light5);
 
-    vector<Light> lights;
-    lights.push_back(light1);
-    lights.push_back(light2);
-    lights.push_back(light3);
-    lights.push_back(light4);
-    lights.push_back(light5);
-    
-    Scene scene = Scene(objects, lights);
-    return scene;
+  Scene scene = Scene(objects, lights);
+  return scene;
 }
 
-void render(Scene scene, Vector3D cameraOrigin){
-    cout << "Engaging render" << endl;
-    // My idea:
-        // We could have a tensor, that is a 2D matrix which would represent each point in the canvas, and it would be assigned it's index. 
-        // That way, after the parallel operation, we could "paint" on the canvas 
-    // #pragma omp parallel for
-    for (int x = -CanvasWidth/2; x < CanvasWidth/2; x++){
-        for (int y = -CanvasHeight/2; y < CanvasHeight/2; y++){
-            Vector3D D = CanvasToViewport(y, -x, ViewportWidth, ViewportHeight, CanvasWidth, CanvasHeight, distance_viewport_camera); // NOTE, the second value is 0, when in Python it's 0.5 
-            D = D/D.magnitude();
-            Vector3D color = TraceRay(cameraOrigin, D, double(distance_viewport_camera), inf, recursion_depth, scene, BACKGROUND_COLOR);
-            color = ColorMax(color);
-            // colorToMap is the temporary which will take the color and map it to the map2D
-            colorToMap = to_string(int(color.getX())) + " " + to_string(int(color.getY())) + " " + to_string(int(color.getZ())) + "   "; 
-            // Since values tend to go to the negatives, we add the offset to map correctly to the map2D 
-            map2D[y + CanvasHeight/2][x + CanvasWidth/2] = colorToMap;
-        }
-    }    
+void render(Scene scene, Vector3D cameraOrigin) {
+  cout << "Engaging render" << endl;
+  // My idea:
+  // We could have a tensor, that is a 2D matrix which would represent each
+  // point in the canvas, and it would be assigned it's index. That way, after
+  // the parallel operation, we could "paint" on the canvas
+
+  for (int x = -CanvasWidth / 2; x < CanvasWidth / 2; x++) {
+#pragma omp parallel for
+    for (int y = -CanvasHeight / 2; y < CanvasHeight / 2; y++) {
+      Vector3D D = CanvasToViewport(
+          y, -x, ViewportWidth, ViewportHeight, CanvasWidth, CanvasHeight,
+          distance_viewport_camera);  // NOTE, the second value is 0, when in
+                                      // Python it's 0.5
+      D = D / D.magnitude();
+      Vector3D color =
+          TraceRay(cameraOrigin, D, double(distance_viewport_camera), inf,
+                   recursion_depth, scene, BACKGROUND_COLOR);
+      color = ColorMax(color);
+      // colorToMap is the temporary which will take the color and map it to the
+      // map2D
+      colorToMap = to_string(int(color.getX())) + " " +
+                   to_string(int(color.getY())) + " " +
+                   to_string(int(color.getZ())) + "   ";
+      // Since values tend to go to the negatives, we add the offset to map
+      // correctly to the map2D
+      map2D[y + CanvasHeight / 2][x + CanvasWidth / 2] = colorToMap;
+    }
+  }
 }
 
 int main() {
-    double x_real = 0.0; 
-    double y_real = 0.0; 
-    double z_real = 0; 
+  // Profiling time
+  struct timespec start_time, end_time;
+  clock_gettime(CLOCK_MONOTONIC, &start_time);
 
-    string fileName;
-    int count = 0;
-    Vector3D color = Vector3D(1, 2, 3);
-    array<Vector3D, 360> circle = circleCoordinates();
+  double x_real = 0.0;
+  double y_real = 0.0;
+  double z_real = 0;
 
-    int prev = -1;
-    int progress = 0;
-    double final_frame = 360*2;
+  string fileName;
+  int count = 0;
+  Vector3D color = Vector3D(1, 2, 3);
+  array<Vector3D, 360> circle = circleCoordinates();
 
-    double result = 0;
-    for(int frame = (360*2) - 1; frame < final_frame; frame++){
-        // Calculate the depth of the camera for the tracking animation
-        result = 0.25*(360.0/(0.5*(frame + 1)));
+  int prev = -1;
+  int progress = 0;
+  double final_frame = 360 * 2;
 
-        if (frame < 360 || result > 0.7){
-            z_real = result;
-        }
+  double result = 0;
+  for (int frame = (360 * 2) - 1; frame < final_frame; frame++) {
+    // Calculate the depth of the camera for the tracking animation
+    result = 0.25 * (360.0 / (0.5 * (frame + 1)));
 
-        // Vector3D cameraOrigin = Vector3D(x_real, y_real, z_real);
-        Vector3D cameraOrigin = Vector3D(1, 1, 1); 
-
-        fileName = "Output/RTX_Test/frame" + to_string(frame) + ".ppm";
-        // Write the PPM header
-        ofstream ppmFile(fileName);
-        ppmFile << "P3\n";         // PPM file format (plain text)
-        ppmFile << CanvasWidth << " " << CanvasHeight << "\n";        // Image dimensions: 3 pixels wide, 2 pixels high
-        ppmFile << "255\n";        // Maximum color value (8-bit)
-
-        cout << "Making Scene" << endl;
-        Scene scene = makeScene(circle[frame % 360], circle[((int)frame + 120) % 360], circle[((int)frame + 240) % 360], frame);
-        cout << "Scene made" << endl;
-        render(scene, cameraOrigin);
-        cout << "Scene Rendered" << endl;
-
-    //     // This nested for loop maps the map2D values to the ppm file. 
-        for (int x = 0; x < CanvasWidth; x++){
-            for (int y = 0; y < CanvasHeight; y++){
-                ppmFile << map2D[y][x];
-            }
-            ppmFile << "\n";
-        }
-
-        progress = int((frame/final_frame)*100);
-        if (progress != prev){
-            cout << "Loading... " << progress << "% done. Rendering frame " << frame << " of " << int(final_frame) << endl;
-            prev = progress;
-        }
+    if (frame < 360 || result > 0.7) {
+      z_real = result;
     }
 
-    cout << "DONE!" << endl;
-    // return 0;
+    // Vector3D cameraOrigin = Vector3D(x_real, y_real, z_real);
+    Vector3D cameraOrigin = Vector3D(1, 1, 1);
 
-}
+    fileName = "../Output/RTX_Test/framerec" + to_string(frame) + ".ppm";
+    // Write the PPM header
+    ofstream ppmFile(fileName);
 
-int mainAUX() {
-    double x_real = 0.0; 
-    double y_real = 0.0; 
-    double z_real = 0; 
+    ppmFile << "P3\n";  // PPM file format (plain text)
+    ppmFile << CanvasWidth << " " << CanvasHeight
+            << "\n";     // Image dimensions: 3 pixels wide, 2 pixels high
+    ppmFile << "255\n";  // Maximum color value (8-bit)
 
-    string fileName;
-    int count = 0;
-    array<Vector3D, 360> circle = circleCoordinates();
+    cout << "Making Scene" << endl;
+    Scene scene =
+        makeScene(circle[frame % 360], circle[((int)frame + 120) % 360],
+                  circle[((int)frame + 240) % 360], frame);
+    cout << "Scene made" << endl;
+    render(scene, cameraOrigin);
+    cout << "Scene Rendered" << endl;
 
-    int prev = -1;
-    int progress = 0;
-    double final_frame = 360*2;
-    // double final_frame = 1;
-
-    double result = 0;
-    for(int frame = 360; frame < final_frame; frame++){
-        // Calculate the depth of the camera for the tracking animation
-        result = 0.25*(360.0/(0.5*(frame + 1)));
-
-        if (frame < 360 || result > 0.7){
-            z_real = result;
-        }
-
-        cout<< "Camera " << z_real<<endl;
-        Vector3D cameraOrigin = Vector3D(x_real, y_real, z_real); 
-        cout << "Creating filename" << endl;
-        fileName = "Output/RTX_Test/frame" + to_string(frame) + ".ppm";
-        // Write the PPM header
-        cout << "ofstream" << endl;
-        ofstream ppmFile(fileName);
-        ppmFile << "P3\n";         // PPM file format (plain text)
-        ppmFile << CanvasWidth << " " << CanvasHeight << "\n";        // Image dimensions: 3 pixels wide, 2 pixels high
-        ppmFile << "255\n";        // Maximum color value (8-bit)
-
-        cout << "Making Scene" << endl;
-        Scene scene = makeScene(circle[frame % 360], circle[((int)frame + 120) % 360], circle[((int)frame + 240) % 360], frame);
-        cout << "Scene made" << endl;
-        render(scene, cameraOrigin);
-        cout << "Scene Rendered" << endl;
-
-        // This nested for loop maps the map2D values to the ppm file. 
-        for (int x = 0; x < CanvasWidth; x++){
-            for (int y = 0; y < CanvasHeight; y++){
-                ppmFile << map2D[y][x];
-            }
-            ppmFile << "\n";
-        }
-
-        progress = int((frame/final_frame)*100);
-        if (progress != prev){
-            cout << "Loading... " << progress << "% done. Rendering frame " << frame << " of " << int(final_frame) << endl;
-            prev = progress;
-        }
+    // This nested for loop maps the map2D values to the ppm file.
+    for (int x = 0; x < CanvasWidth; x++) {
+      for (int y = 0; y < CanvasHeight; y++) {
+        ppmFile << map2D[y][x];
+      }
+      ppmFile << "\n";
     }
 
-    cout << "DONE!" << endl;
-    return 0;
+    progress = int((frame / final_frame) * 100);
+    if (progress != prev) {
+      cout << "Loading... " << progress << " done. Rendering frame " << frame
+           << " of " << int(final_frame) << endl;
+      prev = progress;
+    }
+  }
 
+  // Get end time
+  clock_gettime(CLOCK_MONOTONIC, &end_time);
+  double elapsed = (end_time.tv_sec - start_time.tv_sec) +
+                   (end_time.tv_nsec - start_time.tv_nsec) / 1e9;
+  printf("Execution time: %f seconds\n", elapsed);
+  cout << "DONE!!" << endl;
+  return 0;
 }
+
+// int mainAUX() {
+//   double x_real = 0.0;
+//   double y_real = 0.0;
+//   double z_real = 0;
+
+//   string fileName;
+//   int count = 0;
+//   array<Vector3D, 360> circle = circleCoordinates();
+
+//   int prev = -1;
+//   int progress = 0;
+//   double final_frame = 360 * 2;
+//   // double final_frame = 1;
+
+//   double result = 0;
+//   for (int frame = 360; frame < final_frame; frame++) {
+//     // Calculate the depth of the camera for the tracking animation
+//     result = 0.25 * (360.0 / (0.5 * (frame + 1)));
+
+//     if (frame < 360 || result > 0.7) {
+//       z_real = result;
+//     }
+
+//     cout << "Camera " << z_real << endl;
+//     Vector3D cameraOrigin = Vector3D(x_real, y_real, z_real);
+//     cout << "Creating filename" << endl;
+//     fileName = "Output/RTX_Test/frame" + to_string(frame) + ".ppm";
+//     // Write the PPM header
+//     cout << "ofstream" << endl;
+//     ofstream ppmFile(fileName);
+//     ppmFile << "P3\n";  // PPM file format (plain text)
+//     ppmFile << CanvasWidth << " " << CanvasHeight
+//             << "\n";     // Image dimensions: 3 pixels wide, 2 pixels high
+//     ppmFile << "255\n";  // Maximum color value (8-bit)
+
+//     cout << "Making Scene" << endl;
+//     Scene scene =
+//         makeScene(circle[frame % 360], circle[((int)frame + 120) % 360],
+//                   circle[((int)frame + 240) % 360], frame);
+//     cout << "Scene made" << endl;
+//     render(scene, cameraOrigin);
+//     cout << "Scene Rendered" << endl;
+
+//     // This nested for loop maps the map2D values to the ppm file.
+//     for (int x = 0; x < CanvasWidth; x++) {
+//       for (int y = 0; y < CanvasHeight; y++) {
+//         ppmFile << map2D[y][x];
+//       }
+//       ppmFile << "\n";
+//     }
+
+//     progress = int((frame / final_frame) * 100);
+//     if (progress != prev) {
+//       cout << "Loading... " << progress << "% done. Rendering frame " <<
+//       frame
+//            << " of " << int(final_frame) << endl;
+//       prev = progress;
+//     }
+//   }
+
+//   cout << "DONE!" << endl;
+//   return 0;
+// }
